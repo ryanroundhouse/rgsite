d49fd1e0bc9ea606d1d40d6fa0f86caa6715ab23
Ryan Graham <rg@ryangraham.ca>
2021-04-10 19:22:59 -0400

    more project transition to headless cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,68 +1,118 @@
 const moment = require('moment');
-const pluginRss = require("@11ty/eleventy-plugin-rss");
- 
+const markdownIt = require('markdown-it');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+
 moment.locale('en');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addFilter('dateIso', date => {
-        return moment(date).toISOString();
-    });
-    eleventyConfig.addFilter('dateReadable', date => {
-        return moment(date).utc().format('LL');
-    });
-    eleventyConfig.addFilter("squash", require("./src/_filters/squash.js") );
+  const md = new markdownIt({
+    html: true,
+  });
 
-    eleventyConfig.addCollection("posts", function(collection) {
-      return collection.getFilteredByGlob("src/posts/**/blog/*.md").reverse();
-    });
+  eleventyConfig.addPairedShortcode('markdown', (content) => {
+    return md.render(content);
+  });
 
-    eleventyConfig.addJavaScriptFunction("myResponsiveImage", function(src, options) {
+  eleventyConfig.addFilter('dateIso', (date) => {
+    return moment(date).toISOString();
+  });
+  eleventyConfig.addFilter('dateReadable', (date) => {
+    return moment(date).utc().format('LL');
+  });
+  eleventyConfig.addFilter('squash', require('./src/_filters/squash.js'));
+  // debugger directive to output data
+  eleventyConfig.addFilter('debugger', (...args) => {
+    console.log(...args);
+  });
+
+  eleventyConfig.addCollection('posts', function (collection) {
+    return collection.getFilteredByGlob('src/posts/**/blog/*.md').reverse();
+  });
+
+  eleventyConfig.addJavaScriptFunction(
+    'myResponsiveImage',
+    function (src, options) {
       // returns Promise
       return Image(src, options);
-    });
+    }
+  );
 
-    // add rss plugin
-    eleventyConfig.addPlugin(pluginRss);
+  // add rss plugin
+  eleventyConfig.addPlugin(pluginRss);
 
-    eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
-    
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addPassthroughCopy("./src/webfonts");
-    eleventyConfig.addPassthroughCopy("./favicon.ico");
+  eleventyConfig.addShortcode('excerpt', (article) => extractExcerpt(article));
 
+  eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addPassthroughCopy('./src/webfonts');
+  eleventyConfig.addPassthroughCopy('./favicon.ico');
 
-    return {
-        dir: {
-            input: "src"
-        }
-    };
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+
+    // -----------------------------------------------------------------
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Don’t worry about leading and trailing slashes, we normalize these.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+
+    // Optional (default is shown)
+    pathPrefix: '/',
+    // -----------------------------------------------------------------
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: 'njk',
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: 'njk',
+
+    // Opt-out of pre-processing global data JSON files: (default: `liquid`)
+    dataTemplateEngine: false,
+
+    // These are all optional (defaults are shown):
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
+  };
 };
 
 // extract excerpt from post
 function extractExcerpt(article) {
-    if (!article.hasOwnProperty('templateContent')) {
-      console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-      return null;
-    }
-   
-    let excerpt = null;
-    const content = article.templateContent;
-   
-    // The start and end separators to try and match to extract the excerpt
-    const separatorsList = [
-      { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' }
-    ];
-   
-    separatorsList.some(separators => {
-      const startPosition = content.indexOf(separators.start);
-      const endPosition = content.indexOf(separators.end);
-   
-      if (startPosition !== -1 && endPosition !== -1) {
-        excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-        return true; // Exit out of array loop on first match
-      }
-    });
-   
-    return excerpt;
+  if (!article.hasOwnProperty('templateContent')) {
+    console.warn(
+      'Failed to extract excerpt: Document has no property "templateContent".'
+    );
+    return null;
   }
+
+  let excerpt = null;
+  const content = article.templateContent;
+
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+  ];
+
+  separatorsList.some((separators) => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content
+        .substring(startPosition + separators.start.length, endPosition)
+        .trim();
+      return true; // Exit out of array loop on first match
+    }
+  });
+
+  return excerpt;
+}
---
+++ src/_data/projects.js
@@ -0,0 +1,89 @@
+const fetch = require('node-fetch');
+
+// function to get blogposts
+async function getAllProjects() {
+  // max number of records to fetch per query
+  const recordsPerQuery = 100;
+
+  // number of records to skip (start at 0)
+  let recordsToSkip = 0;
+
+  let makeNewQuery = true;
+
+  let projects = [];
+
+  // make queries until makeNewQuery is set to false
+  while (makeNewQuery) {
+    try {
+      // initiate fetch
+      const data = await fetch('https://rgsite-cms.herokuapp.com/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          query: `{
+            projects{
+              id,
+              title,
+              slug,
+              description,
+              technologies{
+                name,
+                logo{
+                  url,
+                  alternativeText,
+                  width,
+                  height
+                }
+              }
+            }
+          }`,
+        }),
+      });
+
+      // store the JSON response when promise resolves
+      const response = await data.json();
+
+      // handle CMS errors
+      if (response.errors) {
+        let errors = response.errors;
+        errors.map((error) => {
+          console.log(error.message);
+        });
+        throw new Error('Houston... We have a CMS problem');
+      }
+
+      // update projects array with the data from the JSON response
+      projects = projects.concat(response.data.projects);
+
+      // prepare for next query
+      recordsToSkip += recordsPerQuery;
+
+      // stop querying if we are getting back less than the records we fetch per query
+      if (response.data.projects.length < recordsPerQuery) {
+        makeNewQuery = false;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // format blogposts objects
+  const projectsFormatted = projects.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      technologies: item.technologies,
+      slug: item.slug,
+    };
+  });
+
+  // return formatted blogposts
+  return projectsFormatted;
+}
+
+// export for 11ty
+module.exports = getAllProjects;
